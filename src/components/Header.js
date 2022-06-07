c0f7b8329ea1515a4ae5d4f3e3f2e30011dc384e
mark-escosura <escosuramarkse@gmail.com>
2022-06-07 14:31:43 -0700

    WIP: Applied responsive styling throughout different breakpoints

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,30 @@ import { Link } from 'react-scroll';
 
 const Header = () => {
   return (
-    <header id='header' className='header_container'>
-      <h1>MIT</h1>
+    <header id='header'>
       <nav>
-        <Link
-          activeClass='active'
-          smooth
-          spy
-          duration={1850}
-          offset={-100}
-          to='hero'
-        >
-          Home
-        </Link>
-        <Link activeClass='active' smooth spy duration={1850} to='experience'>
-          Experience
-        </Link>
-        <Link activeClass='active' smooth spy duration={1850} to='projects'>
-          Projects
-        </Link>
-        <Link smooth spy duration={1850} to='contact'>
-          <button className='hire_me_button'>Hire Me</button>
-        </Link>
+        <h1>MIT</h1>
+        <div className='links_container'>
+          <Link
+            activeClass='active'
+            smooth
+            spy
+            duration={1850}
+            offset={-100}
+            to='hero'
+          >
+            Home
+          </Link>
+          <Link activeClass='active' smooth spy duration={1850} to='experience'>
+            Experience
+          </Link>
+          <Link activeClass='active' smooth spy duration={1850} to='projects'>
+            Projects
+          </Link>
+          <Link smooth spy duration={1850} to='contact'>
+            <button className='hire_me_button'>Hire Me</button>
+          </Link>
+        </div>
       </nav>
     </header>
   );
